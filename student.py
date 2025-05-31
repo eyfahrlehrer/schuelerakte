7773c9c52af7ecb99b2831cca22faa43b4493126
eyfahrlehrer <info@machmichmobiler.com>
2025-05-31 18:01:08 +0200

    Create student.py

---
+++ student.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Schülerübersicht</title>
+</head>
+<body>
+    <h1>📋 Schülerakte</h1>
+    <ul>
+        {% for s in schueler %}
+            <li>
+                <strong>{{ s.name }}</strong> – {{ s.progress }}
+            </li>
+        {% endfor %}
+    </ul>
+
+    <a href="/start">Zurück zur Startseite</a>
+</body>
+</html>
