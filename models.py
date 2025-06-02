7961c0ffdd82fdc37565602208a43889a4dfa756
eyfahrlehrer <info@machmichmobiler.com>
2025-06-02 20:03:01 +0200

    Update models.py

--- models.py
+++ models.py
@@ -4,14 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Fahrschueler(Base):
-    __tablename__ = "fahrschueler"  # Name der Tabelle in der Datenbank
+    __tablename__ = "fahrschueler"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    vorname = Column(String)
-    geburtsdatum = Column(String)  # Oder Date, wenn du die Umwandlung in app.py sicherstellst
+    name = Column(String, nullable=False)
+    vorname = Column(String, nullable=False)
+    geburtsdatum = Column(String, nullable=False)  # Oder Date, wenn du mit Datumsformat arbeitest
     strasse = Column(String)
     hausnummer = Column(String)
     mobil = Column(String)
-    sehhilfe = Column(Boolean)
-    theorie_bestanden = Column(Boolean)
+    sehhilfe = Column(Boolean, default=False)
+    theorie_bestanden = Column(Boolean, default=False)
