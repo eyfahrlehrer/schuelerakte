ba0898c6a49c6cf73ef040ca4b31fd0201753bb8
eyfahrlehrer <info@machmichmobiler.com>
2025-06-03 01:37:59 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -2,11 +2,11 @@ import os
 from sqlalchemy import create_engine
 from models import Base
 
-# Holt die URL aus Railway-Umgebungsvariablen
+# Holt die Datenbank-URL aus der Railway-Umgebungsvariable
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-print("🚀 Starte Datenbanktabellen-Erstellung ...")
+print("📡 Starte Datenbanktabellen-Erstellung ...")
 Base.metadata.create_all(engine)
 print("✅ Tabellen erfolgreich erstellt.")
