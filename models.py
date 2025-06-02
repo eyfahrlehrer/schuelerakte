638cfff9895cc41d181b6d746126225fc05934cf
eyfahrlehrer <info@machmichmobiler.com>
2025-06-02 21:25:11 +0200

    Update models.py

--- models.py
+++ models.py
@@ -15,3 +15,21 @@ class Fahrschueler(Base):
     mobil = Column(String)
     sehhilfe = Column(Boolean, default=False)
     theorie_bestanden = Column(Boolean, default=False)
+
+class Grundstufe(Base):
+    __tablename__ = "grundstufe"
+
+    id = Column(Integer, primary_key=True, index=True)
+    schueler_id = Column(Integer, nullable=False)
+
+    sitz = Column(Boolean, default=False)
+    spiegel = Column(Boolean, default=False)
+    lenkrad = Column(Boolean, default=False)
+    kopfstuetze = Column(Boolean, default=False)
+    einweisung = Column(Boolean, default=False)
+    pedale = Column(Boolean, default=False)
+    gang = Column(Boolean, default=False)
+    sicht = Column(Boolean, default=False)
+
+    schaltuebung = Column(String, nullable=True)
+    notizen = Column(String, nullable=True)
