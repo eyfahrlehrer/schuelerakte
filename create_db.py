ce8c022bc32effd6aa008b4da2258aeacb544791
eyfahrlehrer <info@machmichmobiler.com>
2025-06-04 18:54:42 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,13 +1,11 @@
 import os
 from sqlalchemy import create_engine
-from models import Base
-import models  # <<< wichtig, damit SQLAlchemy alle Models kennt
+from models import Base, Fahrschueler, Grundstufe, Aufbaustufe, Grundfahraufgaben, Ueberlandfahrt, Daemmerung, Reifestufe, Technik
 
-# Holt die Datenbank-URL aus den Railway-Umgebungsvariablen
+# Holt die Datenbank-URL aus der Railway-Umgebungsvariable
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL)
 
-print("⚙️ Starte Datenbanktabellen-Erstellung ...")
+print("🚀 Starte Datenbanktabellen-Erstellung ...")
 Base.metadata.create_all(engine)
 print("✅ Tabellen erfolgreich erstellt.")
